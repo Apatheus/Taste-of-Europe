87e041a23483b58b30ae5c83bfd29630ce3bb7d4
charlesgispert <116556941+charlesgispert@users.noreply.github.com>
2022-10-24 17:10:29 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,95 @@
+<!--connection BDD-->
+<?php
+       //définition connection et requete
+       $servername = 'localhost';
+       $username = 'root';
+       $password = '';
+       //connexion bdd et requete
+       //On établit la connexion
+       $connection = new PDO("mysql:host=$servername;port=3306;dbname=tasteofeurope", $username, $password);
+       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Taste of Europe</title>
+</head>
+
+<body>
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <nav class="nav justify-content-center bg-light p-2 ">
+        <a class="nav-link active" href="#">Accueil</a></li>
+        <a class="nav-link" href="#">Programme</a></li>
+        <a class="nav-link" href="#"><img src="">(Logo)</a></li>
+        <a class="nav-link" href="tickets.php">Tickets</a></li>
+        <a class="nav-link" href="connection.php">Administration</a></li>
+    </nav>
+<section class=ogin class="element3">
+  <?php
+    
+    $msg = "";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $name = $_POST["name"];
+       $password = ($_POST["password"]);
+     if ($name == '' || $password == '') {
+          $msg = "You must enter all fields";
+      } else {
+          $requete= "SELECT * FROM admin WHERE login = '$name' AND mdp = '$password'";
+          $resultats=$connection->query($requete);
+          $tabMembers=$resultats->fetchAll();
+          $resultats->closeCursor();
+          if (count($tabMembers)==0) {
+              echo "Could not successfully run query ($sql) from DB: " ;
+              exit;
+          }
+
+
+      }
+  }
+  ?>
+
+   <fieldset class="bibou">
+      <form name="frmregister" action="admin.php" method="post"  class="bibou" >
+  		<table class="form" border="0">
+
+  			<tr>
+  			<td></td>
+  				<td style="color:red;">
+  				<?php echo $msg; ?></td>
+  			</tr>
+
+  			<tr>
+  				<th><label for="name"><strong>Name:</strong></label></th>
+  				<td><input class="inp-text" name="name" id="name" type="text" size="30" /></td>
+  			</tr>
+  			<tr>
+  				<th><label for="name"><strong>Password:</strong></label></th>
+  				<td><input class="inp-text" name="password" id="password" type="password" size="30" /></td>
+  			</tr>
+  			<tr>
+  			<td></td>
+  				<td class="submit-button-right">
+  				<input class="send_btun" type="submit" value="Submit" alt="Submit" title="Submit" />
+
+  				<input class="send_btn" type="reset" value="Reset" alt="Reset" title="Reset"/></td>
+
+  			</tr>
+  		</table>
+
+  	</form>
+ </fieldset>
+
+    </section>
+</body>
+</html>
---
+++ tickets.php
@@ -0,0 +1,83 @@
+<!--connection BDD-->
+<?php
+       //définition connection et requete
+       $servername = 'localhost';
+       $username = 'root';
+       $password = '';
+       //connexion bdd et requete
+       //On établit la connexion
+       $connection = new PDO("mysql:host=$servername;port=3306;dbname=tasteofeurope", $username, $password);
+       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<!--récupération et envoie de donées-->
+<?php
+    if(isset($_POST['nom'])){
+     $nom=$_POST['nom'];
+     $prenom=$_POST['prenom'];
+     $mail=$_POST['mail'];
+     $animation=$_POST['animation'];
+     $sql0 ="INSERT INTO inscrit (nom, prenom, mail, animation) VALUES ('$nom' , '$prenom', '$mail','$animation')";
+     $connection->exec($sql0);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Taste of Europe</title>
+</head>
+
+<body>
+    <nav class="nav justify-content-center bg-light p-2 ">
+        <a class="nav-link active" href="#">Accueil</a></li>
+        <a class="nav-link" href="#">Programme</a></li>
+        <a class="nav-link" href="#"><img src="">(Logo)</a></li>
+        <a class="nav-link" href="tickets.php">Tickets</a></li>
+        <a class="nav-link" href="connection.php">Administration</a></li>
+    </nav>
+
+    <div class="grid"> <!-- Début Grille -->
+        <h1 class="d-flex justify-content-center mt-5">Tickets</h1>
+
+
+        <!--formulaire a pas toucher-->
+        <form  action="tickets.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border">
+            <legend>Inscription aux activités :</legend>
+                <div class="form-group mt-2">
+                    <label for="nom" >Nom :</label>
+                    <input name="nom" type="text" class="form-control" placeholder="Votre nom">
+                </div>
+
+                <div class="form-group mt-2">
+                    <label for="prenom" >Prénom :</label>
+                    <input name="prenom" type="text" class="form-control" placeholder="Votre prénom">
+                </div>
+
+                <div class="form-group mt-2 mb-2">
+                    <label for="mail" >Adresse mail :</label>
+                    <input name="mail" type="mail" class="form-control" placeholder="Votre adresse mail">
+                </div>
+                <div>
+                <label for="animation">animation</label>
+              <select name="animation"  >
+                <option value="concour de cuisine">concour de cuisine</option>
+                <option value="concour de mangeur"> concour de mangeur</option>
+              </select><br></div>
+                
+
+                <div class="text-center"><br>
+                    <input type="submit" value="Envoyer" class="btn btn-warning">
+                </div>
+        </form>
+    </div> <!-- Fin Grille -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
