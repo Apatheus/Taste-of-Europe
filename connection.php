4385582da452e41a36b30866a9c9350f200be0af
charlesgispert <116556941+charlesgispert@users.noreply.github.com>
2022-10-25 10:48:38 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,18 +1,19 @@
 <!--connection BDD-->
 <?php
-       //définition connection et requete
-       $servername = 'localhost';
-       $username = 'root';
-       $password = '';
-       //connexion bdd et requete
-       //On établit la connexion
-       $connection = new PDO("mysql:host=$servername;port=3306;dbname=tasteofeurope", $username, $password);
-       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//définition connection et requete
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+//connexion bdd et requete
+//On établit la connexion
+$connection = new PDO("mysql:host=$servername;port=3306;dbname=tasteofeurope", $username, $password);
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,70 +25,77 @@
 </head>
 
 <body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     <nav class="nav justify-content-center bg-light p-2">
-            <a class="nav-link link-dark active mx-2" href="#">Accueil</a>
-            <a class="nav-link link-dark mx-2" href="#">Programme</a>
-            <a class="nav-link link-dark mx-2" href="#"><img class="img-fluid" src="images/logo.png"></a>
-            <a class="nav-link link-dark mx-2" href="#">Tickets</a>
-            <a class="nav-link link-dark mx-2" href="#">Administration</a>
+        <a class="nav-link link-dark mx-2" href="index.html">Accueil</a>
+        <a class="nav-link link-dark mx-2" href="programme.html">Programme</a>
+        <a class="nav-link link-dark mx-2" href="index.html"><img class="img-fluid" src="images/logo.png"></a>
+        <a class="nav-link link-dark mx-2" href="tickets.php">Tickets</a>
+        <a class="nav-link link-dark mx-2" href="connection.php">Administration</a>
     </nav>
 
-<section class=ogin class="element3">
-  <?php
-    
-    $msg = "";
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $name = $_POST["name"];
-       $password = ($_POST["password"]);
-     if ($name == '' || $password == '') {
-          $msg = "You must enter all fields";
-      } else {
-          $requete= "SELECT * FROM admin WHERE login = '$name' AND mdp = '$password'";
-          $resultats=$connection->query($requete);
-          $tabMembers=$resultats->fetchAll();
-          $resultats->closeCursor();
-          if (count($tabMembers)==0) {
-              echo "Could not successfully run query ($sql) from DB: " ;
-              exit;
-          }
+    <section class=ogin class="element3">
+        <?php
 
+        $msg = "";
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $name = $_POST["name"];
+            $password = ($_POST["password"]);
+            if ($name == '' || $password == '') {
+                $msg = "You must enter all fields";
+            } else {
+                $requete = "SELECT * FROM admin WHERE login = '$name' AND mdp = '$password'";
+                $resultats = $connection->query($requete);
+                $tabMembers = $resultats->fetchAll();
+                $resultats->closeCursor();
+                if (count($tabMembers) == 0) {
+                    echo "Could not successfully run query ($sql) from DB: ";
+                    exit;
+                }
+            }
+        }
+        ?>
 
-      }
-  }
-  ?>
+        <fieldset class="bibou">
+            <form name="frmregister" action="admin.php" method="post" class="bibou">
+                <table class="form" border="0">
 
-   <fieldset class="bibou">
-      <form name="frmregister" action="admin.php" method="post"  class="bibou" >
-  		<table class="form" border="0">
+                    <tr>
+                        <td></td>
+                        <td style="color:red;">
+                            <?php echo $msg; ?></td>
+                    </tr>
 
-  			<tr>
-  			<td></td>
-  				<td style="color:red;">
-  				<?php echo $msg; ?></td>
-  			</tr>
+                    <tr>
+                        <th><label for="name"><strong>Name:</strong></label></th>
+                        <td><input class="inp-text" name="name" id="name" type="text" size="30" /></td>
+                    </tr>
+                    <tr>
+                        <th><label for="name"><strong>Password:</strong></label></th>
+                        <td><input class="inp-text" name="password" id="password" type="password" size="30" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td class="submit-button-right">
+                            <input class="send_btun" type="submit" value="Submit" alt="Submit" title="Submit" />
 
-  			<tr>
-  				<th><label for="name"><strong>Name:</strong></label></th>
-  				<td><input class="inp-text" name="name" id="name" type="text" size="30" /></td>
-  			</tr>
-  			<tr>
-  				<th><label for="name"><strong>Password:</strong></label></th>
-  				<td><input class="inp-text" name="password" id="password" type="password" size="30" /></td>
-  			</tr>
-  			<tr>
-  			<td></td>
-  				<td class="submit-button-right">
-  				<input class="send_btun" type="submit" value="Submit" alt="Submit" title="Submit" />
+                            <input class="send_btn" type="reset" value="Reset" alt="Reset" title="Reset" />
+                        </td>
 
-  				<input class="send_btn" type="reset" value="Reset" alt="Reset" title="Reset"/></td>
+                    </tr>
+                </table>
 
-  			</tr>
-  		</table>
-
-  	</form>
- </fieldset>
+            </form>
+        </fieldset>
 
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <footer class="p-4 bg-dark text-white text-center">
+        Conçu par les MMI2 du Puy-en-Velay, année 2022-2023.
+    </footer>
+
 </body>
+
 </html>
