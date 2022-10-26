4fd903e065febc21d915fcd428637c0f92df8fe1
Arthur Cros <115494377+arthurcros@users.noreply.github.com>
2022-10-26 12:15:08 +0200

    Ajout du dark mode
    Dark mode automatique, changement de couleurs a partire de variable sur la page index, page programme ne fonctionne pas.
    Html : correction certaines parties, ajout de boutons, meta pour adapter couler android, lib FA
    CSS: changement logo, adaptation de la taille correct, adaptation+design elements, image, carte; animations
    JS : ajout dark mode + automatique

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ var circle = L.circle([45.043092230725975, 3.8836065281294774], {
 }).addTo(map);
 
 // Fin Leaflet
+// Début dark mode
+const darkMode = document.getElementById('dark-mode');
+
+darkMode.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+});
+// Dark mode automatique
+const isDarkMode = () =>{
+if (globalThis.matchMedia?.("(prefers-color-scheme:dark)").matches ?? true) {
+    document.body.classList.toggle('dark');
+    document.getElementById("dark-mode").checked = true;
+}}
+// Usage
+isDarkMode();
+// fin dark mode
