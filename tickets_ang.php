a8364c5f2467cabda092e8e7f0d6e61c06d7374d
paulbruey <116547584+paulbruey@users.noreply.github.com>
2022-10-27 17:20:12 +0200

    Update tickets_ang.php

--- tickets_ang.php
+++ tickets_ang.php
@@ -1,23 +1,19 @@
-<!--connection BDD-->
 <?php
-//définition connection et requete
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-//connexion bdd et requete
-//On établit la connexion
-$connection = new PDO("mysql:host=$servername;port=3306;dbname=tasteofeurope", $username, $password);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require "connexion_bdd.php";
+require "config.inc.php";
+$connection = new BDD(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
+$link = $connection->connexion();
 ?>
+
 <!--récupération et envoie de donées-->
 <?php
 if (isset($_POST['nom'])) {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $mail = $_POST['mail'];
-    $animation = $_POST['animation'];
-    $sql0 = "INSERT INTO inscrit (nom, prenom, mail, animation) VALUES ('$nom' , '$prenom', '$mail','$animation')";
-    $connection->exec($sql0);
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $mail = $_POST['mail'];
+  $animation = $_POST['animation'];
+  $sql0 = "INSERT INTO inscrit (nom, prenom, mail, animation) VALUES ('$nom' , '$prenom', '$mail','$animation')";
+  mysqli_query($link, $sql0);
 }
 ?>
 
@@ -25,72 +21,77 @@ if (isset($_POST['nom'])) {
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" media="(prefers-color-scheme: light)" content="white">
+  <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="script.js"></script>
+  <link href="style.css" rel="stylesheet">
+  <!-- CSS FontAwsome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- CSS Flags Lib -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"/>
 
-    <title>Taste of Europe</title>
-    <style>
-        body {
-            background-image: url(https://dbdzm869oupei.cloudfront.net/img/vinylrugs/preview/29739.png);
+  <title>Taste of Europe</title>
 
-
-        }
-    </style>
 </head>
 
-<body>
-    <?php
-  include('header.html');
-  ?>
+<body id="bodytickets">
+  <?php include ('header_ang.html') ?>
 
 
-    <div class="grid">
-        <!-- Début Grille -->
-        <h1 class="d-flex justify-content-center mt-5 ">Tickets</h1>
+
+  <div class="grid">
+    <!-- Début Grille -->
+    <h1 class="d-flex justify-content-center mt-5 ">Tickets</h1>
 
 
-        <!--formulaire a pas toucher-->
-        <form action="tickets.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border bg-light rounded ">
-            <legend>Registration for activities :</legend>
-            <div class="form-group mt-2">
-                <label for="nom">Name :</label>
-                <input name="nom" type="text" class="form-control" placeholder="Votre nom">
-            </div>
+    <!--formulaire a pas toucher-->
+    <form action="tickets.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border bg-light rounded ">
+      <legend>Registration for activities :</legend>
+      <div class="form-group mt-2">
+        <label for="nom">Name :</label>
+        <input name="nom" type="text" class="form-control" placeholder="Your name">
+      </div>
 
-            <div class="form-group mt-2">
-                <label for="prenom">First name :</label>
-                <input name="prenom" type="text" class="form-control" placeholder="Votre prénom">
-            </div>
+      <div class="form-group mt-2">
+        <label for="prenom">Last name :</label>
+        <input name="prenom" type="text" class="form-control" placeholder="Your last name">
+      </div>
 
-            <div class="form-group mt-2 mb-2">
-                <label for="mail">E-mail address :</label>
-                <input name="mail" type="mail" class="form-control" placeholder="Votre adresse mail">
-            </div>
-            <div>
-                <label for="animation">Animation</label>
-                <select name="animation">
-                    <option value="concour de cuisine">Cooking contest</option>
-                    <option value="concour de mangeur">Blind tasting</option>
-                </select><br>
-            </div>
-
-
-            <div class="text-center"><br>
-                <input type="submit" value="Envoyer" class="btn btn-warning">
-            </div>
-        </form></br></br></br>
-    </div> <!-- Fin Grille -->
+      <div class="form-group mt-2 mb-2">
+        <label for="mail">E-mail address :</label>
+        <input name="mail" type="mail" class="form-control" placeholder="Your E-mail address">
+      </div>
+      <div>
+        <label for="animation">Animation</label>
+        <select name="animation">
+          <option value="concour de cuisine">Cooking contest</option>
+          <option value="concour de mangeur">Blind tasting</option>
+        </select><br>
+      </div>
 
 
-    <footer class="p-4 bg-dark text-white text-center">
-        Designed by the MMI2 du Puy-en-Velay, year 2022-2023
-    </footer>
+      <div class="text-center"><br>
+        <input type="submit" value="Submit" class="btn btn-warning">
+      </div>
+    </form></br></br></br>
+  </div> <!-- Fin Grille -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
+
+  <footer class="p-4 bg-dark text-white text-center fixed-bottom">
+   Design by MMI2 of Puy-en-Velay, 2022-2023.
+  </footer>
+  <!-- Script Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"></script>
+  <!-- Mes scripts -->
+  <script src="script.js"></script>
 </body>
 
 </html>
