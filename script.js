b66b0218c56d52ef139d66ae9a4af78b8edad719
Apatheus <116081890+Apatheus@users.noreply.github.com>
2022-10-28 07:49:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,18 +45,33 @@ function myFunction1() {
   // Fin fonction cacher
 
 // Leaflet
-var map = L.map('map').setView([45.043088544966466, 3.8836032652867454], 16);
+var map = L.map('map').setView([45.03968506707314, 3.883328892219707], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var circle = L.circle([45.043092230725975, 3.8836065281294774], {
+var circle1 = L.circle([45.04346189481585, 3.8841485736069696], {
     color: 'orange',
     fillColor: 'orange',
     fillOpacity: 0.2,
-    radius: 150
+    radius: 180
 }).addTo(map);
+
+var circle2 = L.circle([45.03968506707314, 3.883328892219707], {
+  color: 'cyan',
+  fillColor: 'cyan',
+  fillOpacity: 0.2,
+  radius: 120
+}).addTo(map);
+
+var circle3 = L.circle([45.04140131889955, 3.883958293165474], {
+  color: 'yellow',
+  fillColor: 'yellow',
+  fillOpacity: 0.2,
+  radius: 80
+}).addTo(map);
+
 
 // Fin Leaflet
