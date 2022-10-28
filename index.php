6956ec7bb2dfd090f477e0fa172c4b53e1156fad
Apatheus <116081890+Apatheus@users.noreply.github.com>
2022-10-28 07:51:22 +0200

    Ajout image index

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
     <div class="container">
         <div class="row">
             <div class="offset-md-2 col-md-4">
-                <img class="img-fluid" id="rounded" src="images/food1.jpg">
+                <img class="img-fluid" id="rounded" src="images/le-poivrier">
             </div>
             <div class="col-md-4">
 
