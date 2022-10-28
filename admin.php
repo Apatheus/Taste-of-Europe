c6df7970890757dbc01c14ad7fc3f00923f060aa
Apatheus <116081890+Apatheus@users.noreply.github.com>
2022-10-28 23:25:17 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -70,22 +70,21 @@ if (isset($_POST['id_am2'])) {
   <meta name="theme-color" media="(prefers-color-scheme: light)" content="white">
   <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black">
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="script.js"></script>
   <link href="style.css" rel="stylesheet">
   <!-- CSS FontAwsome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <!-- CSS Flags Lib -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css" />
 
   <title>Taste of Europe</title>
 
 </head>
 
-<body id="bodyadmin" >
+<body id="bodyadmin">
   <?php
-  include ('header.html');
+  include('header.html');
   ?>
 
   <!--connectionadmin-->
@@ -108,160 +107,205 @@ if (isset($_POST['id_am2'])) {
     }
   }
   ?>
-  <div class="fond2 text-center my-4">
-    <button class="btn" onclick="myFunction1()">Ajout</button>
-    <button class="btn" onclick="myFunction2()">supression</button>
-    <button class="btn" onclick="myFunction3()">modifier</button>
+  <div class="fond2 text-center my-4 offset-md-2 col-md-8">
+    <div class="row text-center my-4">
+      <h5>Cette page sert à ajouter, supprimer ou modifier les activités présentes dans la base de données.</h5>
+    </div>
+
+    <div class="accordion" id="accordionExample">
 
 
-  </br><div id="form1">
-    <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border bg-light rounded 3">
-      <legend> Ajout d'une activité</legend>
-      <div class="form-group mt-2">
-        <label for="id_a">ID :</label>
-        <input name="id_a" type="text" class="form-control" placeholder="id de l'activité">
+      <div class="accordion-item">
+        <!-- Début section 1 -->
+        <h2 class="accordion-header" id="headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <strong>Ajouter une activité</strong>
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div class="accordion-body bg-secondary">
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border bg-light ">
+                <legend> Ajout d'une activité</legend>
+                <div class="form-group mt-2">
+                  <label for="id_a">ID :</label>
+                  <input name="id_a" type="text" class="form-control" placeholder="id de l'activité">
+                </div>
+
+                <div class="form-group mt-2">
+                  <label for="nom">Nom :</label>
+                  <input name="nom" type="text" class="form-control" placeholder="nom activité">
+                </div>
+
+                <div class="form-group mt-2 mb-2">
+                  <label for="desc">Description :</label>
+                  <input name="desc" type="text" class="form-control" placeholder="description">
+                </div>
+                <div>
+                  <label for="date">Date (jj/mm/aa 00h00) :</label>
+                  <input name="date" type="text" class="form-control" placeholder="date">
+                </div>
+
+
+                <div class="text-center"><br>
+                  <input type="submit" value="Envoyer" class="btn">
+                </div>
+              </form></br>
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
+                <legend> Ajout d'une animation</legend>
+                <div class="form-group mt-2">
+                  <label for="id_anim">ID :</label>
+                  <input name="id_anim" type="text" class="form-control" placeholder="id de l'animation">
+                </div>
+
+                <div class="form-group mt-2">
+                  <label for="nom_a">Nom :</label>
+                  <input name="nom_a" type="text" class="form-control" placeholder="nom animation">
+                </div>
+
+                <div class="form-group mt-2 mb-2">
+                  <label for="desc_a">Description :</label>
+                  <input name="desc_a" type="text" class="form-control" placeholder="description">
+                </div>
+                <div>
+                  <label for="date_a">Date (jj/mm/aa 00h00) :</label>
+                  <input name="date_a" type="text" class="form-control" placeholder="date">
+                </div>
+
+                <div class="text-center"><br>
+                  <input type="submit" value="Envoyer" class="btn">
+                </div>
+              </form>
+
+          </div>
+        </div>
+      </div> <!-- Fin section 1 -->
+
+
+
+
+
+      <div class="accordion-item">
+        <!-- Début section 2 -->
+        <h2 class="accordion-header" id="headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <strong>Supprimer une activité</strong>
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div class="accordion-body bg-secondary">
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
+                <legend>Supprimer une activité</legend>
+                <div class="form-group mt-2">
+                  <label for="id_supr">ID de l'activité :</label>
+                  <input name="id_supr" type="text" class="form-control" placeholder="id de l'activité">
+
+                  <div class="text-center"><br>
+                    <input type="submit" value="Envoyer" class="btn">
+                  </div>
+                </div>
+              </form></br>
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
+                <legend>Supprimer une animation</legend>
+                <div class="form-group mt-2">
+                  <label for="id_supr2">ID de l'animation :</label>
+                  <input name="id_supr2" type="text" class="form-control" placeholder="id de l'animation">
+
+                  <div class="text-center"><br>
+                    <input type="submit" value="Envoyer" class="btn">
+                  </div>
+                </div>
+              </form>
+
+          </div>
+        </div>
+      </div> <!-- Fin section 2 -->
+
+
+
+
+
+      <div class="accordion-item">
+        <!-- Début section 3 -->
+        <h2 class="accordion-header" id="headingThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <strong>Modifier une activité</strong>
+          </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div class="accordion-body bg-secondary">
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
+                <legend> Modifier une activité</legend>
+                <div class="form-group mt-2">
+                  <label for="id_am">ID de l'activité à modifier :</label>
+                  <input name="id_am" type="text" class="form-control" placeholder="id de l'activité">
+                </div>
+
+                <div class="form-group mt-2">
+                  <label for="nomm">Nom :</label>
+                  <input name="nomm" type="text" class="form-control" placeholder="nom activité">
+                </div>
+
+                <div class="form-group mt-2 mb-2">
+                  <label for="descm">Description :</label>
+                  <input name="descm" type="text" class="form-control" placeholder="description">
+                </div>
+                <div>
+                  <label for="datem">Date (jj/mm/aa 00h00) :</label>
+                  <input name="datem" type="text" class="form-control" placeholder="date">
+                </div>
+
+
+                <div class="text-center"><br>
+                  <input type="submit" value="Envoyer" class="btn">
+                </div>
+              </form></br>
+
+              <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
+                <legend>Modifier une animation</legend>
+                <div class="form-group mt-2">
+                  <label for="id_am2">ID de l'animation à modifier</label>
+                  <input name="id_am2" type="text" class="form-control" placeholder="id de l'animation">
+                </div>
+
+                <div class="form-group mt-2">
+                  <label for="nomm2">Nom :</label>
+                  <input name="nomm2" type="text" class="form-control" placeholder="nom animation">
+                </div>
+
+                <div class="form-group mt-2 mb-2">
+                  <label for="descm2">Description :</label>
+                  <input name="descm2" type="text" class="form-control" placeholder="description">
+                </div>
+                <div>
+                  <label for="datem2">Date (jj/mm/aa 00h00) :</label>
+                  <input name="datem2" type="text" class="form-control" placeholder="date">
+                </div>
+
+
+                <div class="text-center"><br>
+                  <input type="submit" value="Envoyer" class="btn">
+                </div>
+              </form>
+
+          </div>
+        </div>
       </div>
-
-      <div class="form-group mt-2">
-        <label for="nom">Nom :</label>
-        <input name="nom" type="text" class="form-control" placeholder="nom activité">
-      </div>
-
-      <div class="form-group mt-2 mb-2">
-        <label for="desc">Description :</label>
-        <input name="desc" type="text" class="form-control" placeholder="description">
-      </div>
-      <div>
-        <label for="date">Date (jj/mm/aa 00h00) :</label>
-        <input name="date" type="text" class="form-control" placeholder="date">
-      </div>
-
-
-      <div class="text-center"><br>
-        <input type="submit" value="Envoyer" class="btn">
-      </div>
-    </form></br>
-
-    <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
-      <legend> Ajout d'une animation</legend>
-      <div class="form-group mt-2">
-        <label for="id_anim">ID :</label>
-        <input name="id_anim" type="text" class="form-control" placeholder="id de l'animation">
-      </div>
-
-      <div class="form-group mt-2">
-        <label for="nom_a">Nom :</label>
-        <input name="nom_a" type="text" class="form-control" placeholder="nom animation">
-      </div>
-
-      <div class="form-group mt-2 mb-2">
-        <label for="desc_a">Description :</label>
-        <input name="desc_a" type="text" class="form-control" placeholder="description">
-      </div>
-      <div>
-        <label for="date_a">Date (jj/mm/aa 00h00) :</label>
-        <input name="date_a" type="text" class="form-control" placeholder="date">
-      </div>
-
-
-      <div class="text-center"><br>
-        <input type="submit" value="Envoyer" class="btn">
-      </div>
-    </form>
+    </div> <!-- Fin section 3 -->
 
   </div>
-</br><div id="form2">
-  <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
-    <legend>Supprimer une activité</legend>
-    <div class="form-group mt-2">
-      <label for="id_supr">ID de l'activité :</label>
-      <input name="id_supr" type="text" class="form-control" placeholder="id de l'activité">
+  <footer class="p-4 bg-dark text-white text-center fixed-bottom">
+    Conçu par les MMI2 du Puy-en-Velay, année 2022-2023.
+  </footer>
 
-      <div class="text-center"><br>
-        <input type="submit" value="Envoyer" class="btn">
-      </div>
-    </div>
-  </form></br>
-
-  <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
-    <legend>Supprimer une animation</legend>
-    <div class="form-group mt-2">
-      <label for="id_supr2">ID de l'animation :</label>
-      <input name="id_supr2" type="text" class="form-control" placeholder="id de l'animation">
-
-      <div class="text-center"><br>
-        <input type="submit" value="Envoyer" class="btn">
-      </div>
-    </div>
-  </form>
-</div>
-
-</br><div id="form3">
-  <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
-    <legend> Modifier une activité</legend>
-    <div class="form-group mt-2">
-      <label for="id_am">ID de l'activité à modifier :</label>
-      <input name="id_am" type="text" class="form-control" placeholder="id de l'activité">
-    </div>
-
-    <div class="form-group mt-2">
-      <label for="nomm">Nom :</label>
-      <input name="nomm" type="text" class="form-control" placeholder="nom activité">
-    </div>
-
-    <div class="form-group mt-2 mb-2">
-      <label for="descm">Description :</label>
-      <input name="descm" type="text" class="form-control" placeholder="description">
-    </div>
-    <div>
-      <label for="datem">Date (jj/mm/aa 00h00) :</label>
-      <input name="datem" type="text" class="form-control" placeholder="date">
-    </div>
-
-
-    <div class="text-center"><br>
-      <input type="submit" value="Envoyer" class="btn">
-    </div>
-  </form></br>
-
-  <form action="admin.php" method="POST" class="p-4 offset-md-4 col-md-4 justify-content-center border border bg-light">
-    <legend>Modifier une animation</legend>
-    <div class="form-group mt-2">
-      <label for="id_am2">ID de l'animation à modifier</label>
-      <input name="id_am2" type="text" class="form-control" placeholder="id de l'animation">
-    </div>
-
-    <div class="form-group mt-2">
-      <label for="nomm2">Nom :</label>
-      <input name="nomm2" type="text" class="form-control" placeholder="nom animation">
-    </div>
-
-    <div class="form-group mt-2 mb-2">
-      <label for="descm2">Description :</label>
-      <input name="descm2" type="text" class="form-control" placeholder="description">
-    </div>
-    <div>
-      <label for="datem2">Date (jj/mm/aa 00h00) :</label>
-      <input name="datem2" type="text" class="form-control" placeholder="date">
-    </div>
-
-
-    <div class="text-center"><br>
-      <input type="submit" value="Envoyer" class="btn">
-    </div>
-  </form>
-</div></br></br>
-</div>
-<footer class="p-4 bg-dark text-white text-center fixed-bottom">
-  Conçu par les MMI2 du Puy-en-Velay, année 2022-2023.
-</footer>
-
-<!-- Script Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-<!-- Mes scripts -->
-<script src="script.js"></script>
+  <!-- Script Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- Mes scripts -->
+  <script src="script.js"></script>
 
 
 
