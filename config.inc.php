4ce88a7fc953b5e77a4dc76b3bcec2096fd52353
Apatheus <116081890+Apatheus@users.noreply.github.com>
2022-11-10 13:33:10 +0100

    Modification navbar (pas fini)

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DB_SERVER', "localhost");
+    define('DB_USER', "dabachelie_europe");
+    define('DB_PASS', "tasteofeurope");
+    define('DB_DATABASE', "dabachelie_europe");
+
+ ?>
